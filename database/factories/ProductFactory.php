628b301cf07de3f6ba1e34c011b6858994236696
Luiz Eduardo <luiztsilva@protonmail.com>
2026-03-18 09:33:10 -0300

    correcao factory produtos

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -3,17 +3,14 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Bezhanov\Faker\Provider\Commerce;
 
 class ProductFactory extends Factory
 {
-    
-
     public function definition(): array
     {
         return [
             // Usando sentence (uma frase curta) ou words (palavras soltas)
-            'description'    => ucfirst($this->faker->catchPhrase()), 
+            'description'    => ucfirst($this->faker->words(3, true)), 
             'brand'          => $this->faker->company(),
             'model'          => $this->faker->bothify('??-###'), // Ex: AB-123
             'size'           => $this->faker->randomElement(['P', 'M', 'G', 'GG', '42', '44']),
